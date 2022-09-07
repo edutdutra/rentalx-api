ad70432effa1dd1d1191fa5ba74ed3db7b4d2856
eduardodutra <edutdutra@gmail.com>
2022-09-07 11:03:48 -0300

    ref: SOLID principle

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import {Router} from "express";
 import {CategoriesRepository} from "../repositories/CategoriesRepository";
+import {CreateCategoryService} from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
     const {name, description} = req.body;
-    const categpruAlreadyExists = categoriesRepository.findByName(name);
 
-    if(categpruAlreadyExists) {
-        return res.status(400).json({error: "Category Already exists!"});
-    }
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-    categoriesRepository.create({name, description});
+    createCategoryService.execute({name, description});
     return res.status(201).send();
 });
 
