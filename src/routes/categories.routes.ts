53a2ca8e139d982a9234e1f978d034e18a5686bb
eduardodutra <edutdutra@gmail.com>
2022-09-08 09:57:16 -0300

    ref: SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,15 @@
 import {Router} from "express";
-import {CategoriesRepository} from "../repositories/CategoriesRepository";
-import {CreateCategoryService} from "../services/CreateCategoryService";
+import {createCategoryController} from "../modules/cars/useCases/createCategory";
+import {listCategoriesController} from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
-    const {name, description} = req.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({name, description});
-    return res.status(201).send();
+    return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get('/', (req, res) => {
-    const all = categoriesRepository.list();
-
-    return res.status(201).json(all);
+    return listCategoriesController.handle(req, res);
 });
 
 export {categoriesRoutes};
